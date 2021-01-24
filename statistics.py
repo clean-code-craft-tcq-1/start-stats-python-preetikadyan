cd03e927ab7cb2cf49fe729cbc06c7288d08c3cd
PREETI <preetikadyan634@gmail.com>
2021-01-24 14:02:27 +0530

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -4,3 +4,21 @@ def calculateStats(numbers):
     return { "avg": sum(numbers)/len(numbers), "min": min(numbers), "max": max(numbers) } 
   else:
     return { "avg": 0, "min": 0, "max": 0 }
+  
+class StatsAlerter: 
+    def  __init__(self,th,class_arr):
+         self.th = th
+         self.class_arr = class_arr
+        
+    def checkAndAlert(self,numbers):  
+      if (max(numbers) >= self.th): 
+         self.class_arr[0].emailSent = True
+         self.class_arr[1].ledGlows = True 
+        
+class EmailAlert:
+    emailSent = True
+    
+class LEDAlert:
+    ledGlows = True 
+    
+    
